d8cb5ae873c02be2393fcb0fbfc494439587c3fa
Clive <simbaclive@gmail.com>
2026-07-18 19:33:42 +0200

    Fix product list element selector
    Updated the selector for the product list element.

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,9 @@ import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
 const dataSource = new ProductData("tents");
-const listElement = document.querySelector(".product-list");
+
+
+const listElement = document.querySelector(".products"); 
 
 const myList = new ProductList("tents", dataSource, listElement);
 
