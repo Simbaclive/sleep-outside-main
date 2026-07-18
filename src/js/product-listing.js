0f6bd8046f5383b055d255ee73b658d2dc79f57a
Clive <simbaclive@gmail.com>
2026-07-18 15:50:51 +0200

    Initialize product list on page load
    Added initialization call for myList to ensure proper setup.

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,8 +1,8 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
-import { loadHeaderFooter, getParam } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 
-loadHeaderFooter();
+
 
 const category = getParam("category");
 const dataSource = new ProductData();
